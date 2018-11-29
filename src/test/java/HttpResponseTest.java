4f08e7c46bb4ce6bb2af59b3aa8a94ef0563c115
mingzzzy <minzzzy@woowahan.com>
2018-11-29 19:12:58 +0900

    chore: Add todo comment

--- src/test/java/HttpResponseTest.java
+++ src/test/java/HttpResponseTest.java
@@ -24,7 +24,7 @@ public class HttpResponseTest {
     @Test
     public void responseCookies() throws Exception {
         HttpResponse response = new HttpResponse(createOutputStream("/Http_Cookie.txt"));
-        response.addHeader("Set-Cookie", "logined=true");
+        response.addHeader("Set-Cookie", "logined=true"); // Todo: 쿠키가 여러개 일때 ; 이걸로 연결되는거 addCookie나 StringBuilder로 하는 거
         response.sendRedirect("/index.html");
     }
 
